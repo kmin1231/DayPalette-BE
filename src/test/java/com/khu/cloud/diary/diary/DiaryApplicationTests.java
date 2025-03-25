2af23ee390ffeb38be55ac684b5d82f305bcbeb6
kmin1231 <kmin91143@gmail.com>
2025-03-26 03:24:38 +0900

    🌱 chore: #1 initialize the spring boot project

---
+++ src/test/java/com/khu/cloud/diary/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khu.cloud.diary.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
