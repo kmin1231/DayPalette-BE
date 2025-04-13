8884cc5c190daf955ea83c4553cb1418734a66c9
kmin1231 <kmin91143@gmail.com>
2025-04-13 19:57:33 +0900

    🐛 fix: #1 correct package structure

--- src/main/java/com/khu/cloud/diary/DiaryApplication.java
+++ src/main/java/com/khu/cloud/diary/DiaryApplication.java
@@ -1,4 +1,4 @@
-package com.khu.cloud.diary.diary;
+package com.khu.cloud.diary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
