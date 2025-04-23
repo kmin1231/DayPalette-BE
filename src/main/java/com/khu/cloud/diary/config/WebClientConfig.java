dd21d0f4fa2f192f36afbf0116fb5c60e11bd6c4
kmin1231 <kmin91143@gmail.com>
2025-04-23 15:25:29 +0900

    ✨ feat(posts): #11 add WebClientConfig

---
+++ src/main/java/com/khu/cloud/diary/config/WebClientConfig.java
@@ -0,0 +1,22 @@
+// config/WebClientConfig.java
+
+package com.khu.cloud.diary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${ai.server.url}")
+    private String aiServerBaseUrl;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(aiServerBaseUrl)
+                .build();
+    }
+}
