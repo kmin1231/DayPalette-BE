421963fa81ecffd0216fa882b9d0e3575ffe84da
kmin1231 <kmin91143@gmail.com>
2025-04-23 15:20:27 +0900

    ✨ feat(posts): #11 add WebClient dependency

---
+++ src/main/java/com/khu/cloud/diary/posts/util/FileNameGenerator.java
@@ -0,0 +1,14 @@
+package com.khu.cloud.diary.posts.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class FileNameGenerator {
+    public static String generateFileName(String userId) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMddHHmmss");
+        String formattedDateTime = LocalDateTime.now().format(formatter);
+
+        String fileName = "user_" + userId + "_" + formattedDateTime + ".png";
+        return fileName;
+    }
+}
