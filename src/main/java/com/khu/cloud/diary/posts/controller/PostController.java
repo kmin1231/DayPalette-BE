b1b840364f5f0d88fe83563cc3f227fe84dfdf47
kmin1231 <kmin91143@gmail.com>
2025-04-28 04:33:00 +0900

    ✨ feat(posts): #11 update PostController

--- src/main/java/com/khu/cloud/diary/posts/controller/PostController.java
+++ src/main/java/com/khu/cloud/diary/posts/controller/PostController.java
@@ -1,11 +1,13 @@
-// posts/controller/PostMineController.java
+// posts/controller/PostController.java
 
 package com.khu.cloud.diary.posts.controller;
 
-import com.khu.cloud.diary.posts.dto.PostMineResponse;
-import com.khu.cloud.diary.posts.service.PostMineService;
+import com.khu.cloud.diary.posts.dto.*;
+import com.khu.cloud.diary.posts.service.*;
 import com.khu.cloud.diary.core.response.ApiResponse;
+
 import lombok.RequiredArgsConstructor;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,14 +16,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/posts")
 @RequiredArgsConstructor
-public class PostMineController {
+public class PostController {
 
     private final PostMineService postmMineService;
+    private final PostDetailService postDetailService;
 
     @GetMapping("/mine")
     public ResponseEntity<ApiResponse<List<PostMineResponse>>> getMyPosts() {
-        
         ApiResponse<List<PostMineResponse>> myPosts = postmMineService.getMyPosts();
         return ResponseEntity.ok(myPosts);
+    }
+
+    @GetMapping("/{postId}")
+    public ApiResponse<PostDetailResponse> getPostDetail(@PathVariable Long postId, HttpServletRequest request) {
+        return postDetailService.getPostDetail(postId, request);
     }
 }
