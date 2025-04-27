f71eec7e34fc9cea65d48f786196f108b9535c46
kmin1231 <kmin91143@gmail.com>
2025-04-27 23:50:23 +0900

    ✨ feat(posts): #11 update PostRepository

--- src/main/java/com/khu/cloud/diary/posts/repository/PostRepository.java
+++ src/main/java/com/khu/cloud/diary/posts/repository/PostRepository.java
@@ -3,10 +3,12 @@
 package com.khu.cloud.diary.posts.repository;
 
 import com.khu.cloud.diary.posts.entity.Post;
+import com.khu.cloud.diary.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    List<Post> findAllByUser(Member user);
 }
