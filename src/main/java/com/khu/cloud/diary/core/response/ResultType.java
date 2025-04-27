7b834efe508037699a37a9320428ab484646e892
kmin1231 <kmin91143@gmail.com>
2025-04-27 23:48:50 +0900

    ✨ feat(core): define API response structure

---
+++ src/main/java/com/khu/cloud/diary/core/response/ResultType.java
@@ -0,0 +1,8 @@
+// core/response/ApiResponse.java
+
+package com.khu.cloud.diary.core.response;
+
+public enum ResultType {
+    SUCCESS,    // successful response
+    ERROR       // error response
+}
