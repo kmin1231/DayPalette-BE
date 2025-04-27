2b46922c62b0ad6907d922802f3a1225acfe274d
kmin1231 <kmin91143@gmail.com>
2025-04-28 05:06:33 +0900

    ✨ feat(posts): #11 implement service layer to update post share status

---
+++ src/main/java/com/khu/cloud/diary/posts/dto/PostShareUpdateRequest.java
@@ -0,0 +1,10 @@
+// posts/dto/PostShareUpdateRequest.java
+
+package com.khu.cloud.diary.posts.dto;
+
+import lombok.Data;
+
+@Data
+public class PostShareUpdateRequest {
+    private boolean isShared;
+}
