9b28651a476086cebbabd8fc9f3af3eda3590ec3
kmin1231 <kmin91143@gmail.com>
2025-05-30 05:44:41 +0900

    🔥 hotfix: #11 allow PATCH method in Spring Security configuration

--- src/main/java/com/khu/cloud/diary/config/SecurityConfig.java
+++ src/main/java/com/khu/cloud/diary/config/SecurityConfig.java
@@ -54,11 +54,11 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList(
-            "http://localhost:8080",
-            "https://daypalette-frontend.vercel.app"
+            "http://localhost:8080"
+            // frontend 배포 후 추가
         ));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-	config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+	    config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
